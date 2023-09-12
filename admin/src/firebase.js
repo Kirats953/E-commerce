1ef15d0fad68d689daf6058e130b0a78890aec06
Kirats953 <109973062+Kirats953@users.noreply.github.com>
2023-09-12 13:29:18 +0530

    admin

---
+++ admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDF5S192PpZwqW3yE1ejVwSJVizZljAFGA",
+  authDomain: "shopify-50e11.firebaseapp.com",
+  projectId: "shopify-50e11",
+  storageBucket: "shopify-50e11.appspot.com",
+  messagingSenderId: "120630150319",
+  appId: "1:120630150319:web:b3ec707131578c29c0005a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
